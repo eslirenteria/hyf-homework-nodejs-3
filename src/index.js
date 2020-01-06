519af46fccacb83854c3f9206989dac250b940c1
Jossue Renteria <jrenteria@unitedvirtualities.com>
2020-01-06 10:38:30 -0700

    Step 2 #4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+let list = ['user1','user7','user6','user5','user4','user3','user2'];
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/users', function(req, res){
+    res.json(list)
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
