90394bc61daeeb87f6c6492d53f167039a77bb74
Jossue Renteria <jrenteria@unitedvirtualities.com>
2020-01-06 10:52:04 -0700

    second commit step 2#4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-let list = ['user1','user7','user6','user5','user4','user3','user2'];
+let list = [];
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/users', function(req, res){
