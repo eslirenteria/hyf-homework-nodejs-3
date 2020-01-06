5b350cbb0dd41ff66304f08e6f87725a126e2723
Jossue Renteria <jrenteria@unitedvirtualities.com>
2020-01-06 11:23:01 -0700

    last commit to merge with branch 3

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const port = 3000;
 let list = [];
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/users', function(req, res){
-    res.json(list)
+app.get('/users', (req, res) => res.json(list));
+app.get('users/:id', (req, res) => {
+    let user = list.find(userc => user.id == req.params.id);
+    res.json(user);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
